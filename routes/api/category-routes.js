4c1a5ace194015306a67c9046e7fa653adaa1414
dfreeman3102 <davidfreeman3102@gmail.com>
2024-05-04 19:48:08 -0400

    added post and put functionality for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,12 +34,31 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(categoryData);
+  } catch (err){
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const updatedCategory = await Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where:{
+        id: req.params.id
+      }
+    }
+  )
+  res.json(updatedCategory);
 });
 
 router.delete('/:id', (req, res) => {
